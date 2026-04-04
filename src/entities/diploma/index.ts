5200036f68b0f019312bf672ae4869fa77a60240
Georgiy <scherbinin.zhora2017@yandex.ru>
2026-04-04 22:45:35 +0300

    feat: add diploma-related queries and types to the diploma entity

--- src/entities/diploma/index.ts
+++ src/entities/diploma/index.ts
@@ -4,6 +4,11 @@ export {
   useUpdateDiplomaStatusMutation,
   useGetDiplomaByIdQuery,
   useLazyGetDiplomaByIdQuery,
+  useGetUserDiplomasQuery,
+  useCreateDiplomaQrTokenMutation,
+  useRevokeDiplomaQrTokenMutation,
+  useGetUserDiplomaTokensQuery,
+  useAttachMyDiplomaMutation,
 } from "./api/diplomaApi";
 
 export type {
@@ -11,10 +16,14 @@ export type {
   ICreateDiplomaItem,
   ICreateDiplomasBatchPayload,
   ICreateDiplomasBatchResponse,
+  ICreateQrTokenPayload,
+  IAttachMyDiplomaResponse,
+  IDiplomaUserToken,
   IUniversityDiploma,
   IUpdateDiplomaStatusPayload,
   IDiploma,
   IUniversityShort,
+  QrTokenType,
 } from "./model/type";
 
 export { DegreeLevel } from "./model/type";
