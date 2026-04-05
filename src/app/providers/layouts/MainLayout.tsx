bca112cf70eaec5b71670a157916952d40602829
Alisa Zavesa <alisa.zavesa@yandex.ru>
2026-04-05 14:53:55 +0300

    fix

--- src/app/providers/layouts/MainLayout.tsx
+++ src/app/providers/layouts/MainLayout.tsx
@@ -4,7 +4,7 @@ import Header from "./components/header/Header";
 
 export default function MainLayout() {
   return (
-    <Box style={{ height: "100dvh", display: "flex", flexDirection: "column" }}>
+    <Box style={{ display: "flex", flexDirection: "column" }}>
       <Header />
       <Box
         component="main"
